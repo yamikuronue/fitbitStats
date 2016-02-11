d72202236d3277f2666996e6aa739b831e0fd526
Yami <yamikuronue@gmail.com>
2016-02-11 17:58:08 -0500

    I don't remmeber how to use middleware apparently

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
+'use strict';
+
 const express = require('express');
 var app = express();
 const session = require('express-session')
 const FitbitApiClient = require("fitbit-node");
 const config = require('./config.json');
 
-//TODO: Make configurable
 let client = new FitbitApiClient(config.consumerKey, config.consumerSecret);
 app.use(session({
-	app.get('/', function (req, res) {
-	  res.sendFile('index.html');
-	});
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true
+});
 
-	app.get('/auth', function (req, res) {
-	 res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://MyFitbitStats.no-ip.org/callback'));
-	});
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
 
-	app.get('/callback', function (req, res) {
-		  client.getAccessToken(req.query.code, 'http://MyFitbitStats.no-ip.org/callback').then(function (result) {
-			client.get("/profile.json", result.access_token).then(function (results) {
-				res.send(results[0]);
-			});
-		}).catch(function (error) {
-			res.send(error);
+app.get('/auth', function (req, res) {
+ res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://MyFitbitStats.no-ip.org/callback'));
+});
+
+app.get('/callback', function (req, res) {
+	  client.getAccessToken(req.query.code, 'http://MyFitbitStats.no-ip.org/callback').then(function (result) {
+		client.get("/profile.json", result.access_token).then(function (results) {
+			res.send(results[0]);
 		});
+	}).catch(function (error) {
+		res.send(error);
 	});
 });
 
